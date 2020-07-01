1a4357b9980c6243579ca8d6b854e6c633b02409
adroitwhiz <adroitwhiz@protonmail.com>
2020-07-01 07:36:27 -0400

    Pass insecureHTTPParser to fetch call
    This allows the spawn-separate-process jankiness to be removed.

--- soundclick.js
+++ soundclick.js
@@ -1,23 +1,50 @@
-const child_process = require('child_process');
-const fs = require('fs').promises;
+const fetch = require('node-fetch');
 
-// SoundClick's CDN (Incapsula) is broken and returns HTTP headers that Node's new parser errors on.
-// Ensure that the legacy HTTP parser is used to parse these.
-// See https://github.com/nodejs/node/issues/27711
-// TODO: Incapsula may have gotten their stuff together since
-const crawlSoundclick = bandID => {
-	return new Promise((resolve, reject) => {
-		child_process.exec(`node --insecure-http-parser soundclick-inner.js ${bandID}`, (error, stdout, stderr) => {
-			if (error !== null) reject(error);
-			if (stdout) console.log(stdout);
-			if (stderr) console.error(stderr);
-			resolve();
-		});
-	}).then(() => {
-		return fs.readFile('soundclick-results.json', {encoding: 'utf-8'});
-	}).then(contents => {
-		return fs.unlink('soundclick-results.json').then(() => JSON.parse(contents));
+const parseAsHTML = require('./util/parse-as-html');
+const titleCase = require('./util/title-case');
+
+const crawlSoundclickPage = (results, pageNumber, bandID) => {
+	return fetch(`https://www.soundclick.com/playerV5/panels/getPlaylist_artist.cfm?bandID=${bandID}&skipOnRequestEnd&currentPage=${pageNumber}`, {
+		// SoundClick's CDN (Incapsula) returns HTTP headers that Node's new parser errors on by default.
+		// Ensure that the "insecure" HTTP parser is used to parse these.
+		// See https://github.com/nodejs/node/issues/27711
+		insecureHTTPParser: true
+	})
+	.then(response => response.text())
+	.then(parseAsHTML).then(domNodes => {
+		let numValidNodes = 0;
+		for (const node of domNodes) {
+			if (node.type !== 'tag' || node.name !== 'div') continue;
+			const result = {
+				name: titleCase(node.attribs['data-songtitle'].replace('*SOLD*', '').trim()),
+				pageUrl: `https://www.soundclick.com/artist/default.cfm?bandID=${bandID}&content=songs`,
+				fileUrl: `https://www.soundclick.com/playerV5/panels/audioStream.cfm?songID=${node.attribs['data-songid']}`,
+				producers: [titleCase(node.attribs['data-bandname'])],
+				// May not work for bands other than Hollywood Legend
+				availableForPurchase: !node.attribs['data-songtitle'].includes('*SOLD*')
+			};
+
+			results.push(result);
+			numValidNodes++;
+		}
+
+		// if we got any valid results, there are still more to be found
+		// if not, we're done
+		if (numValidNodes > 0) return crawlSoundclickPage(results, pageNumber + 1, bandID);
+		return results;
 	});
+};
+
+/**
+ * This function takes the producer's "band ID".
+ * This one's real easy to find-- just look at the URL of the site, e.g.
+ * https://www.soundclick.com/artist/default.cfm?bandID=1240873
+ * No prizes for guessing the band ID there.
+ * @param {string} bandID The producer's "band ID".
+ */
+const crawlSoundclick = bandID => {
+	const results = [];
+	return crawlSoundclickPage(results, 1, bandID).then(() => results);
 };
 
 module.exports = crawlSoundclick;
