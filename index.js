14cb5b6d66d4606c919020b7dd8eff911f2b65ce
adroitwhiz <adroitwhiz@protonmail.com>
2020-07-01 08:55:26 -0400

    Allow limiting crawling to only certain producers

--- index.js
+++ index.js
@@ -9,19 +9,21 @@ const path = require('path');
 
 const titleCase = require('./util/title-case');
 
+const argv = require('yargs-parser')(process.argv.slice(2));
+
 const beatFetches = {
-	'shadowville': crawlShadowville(),
-	'epistra': crawlBeatstars('542', 'store'),
-	'hollywood legend': crawlSoundclick('1240873'),
-	'insanebeatz': crawlAirbit('https://airbit.com/widgets/html5?uid=21599&config=181869'),
-	'tantu': crawlBeatstars('28155', 'store'),
-	'anywaywell': crawlBeatstars('201021', 'musician')
+	'shadowville': () => crawlShadowville(),
+	'epistra': () => crawlBeatstars('542', 'store'),
+	'hollywood legend': () => crawlSoundclick('1240873'),
+	'insanebeatz': () => crawlAirbit('https://airbit.com/widgets/html5?uid=21599&config=181869'),
+	'tantu': () => crawlBeatstars('28155', 'store'),
+	'anywaywell': () => crawlBeatstars('201021', 'musician')
 		.then(results =>
 			results.map(beat => {
 				beat.name = beat.name.replace('(Buy 1 Get 3 Free)', '').trim();
 				return beat;
 			})),
-	'kustom': crawlSoundgine('@kustommike').then(results => {
+	'kustom': () => crawlSoundgine('@kustommike').then(results => {
 		return fs.readFile(path.join(__dirname, 'kustom_legacy.json'))
 		.then(contents => {
 			// merge new Kustom results with "legacy" results from old site
@@ -37,9 +39,9 @@ const beatFetches = {
 			return results;
 		});
 	}),
-	'blackrose': crawlBeatstars('174557', 'musician'),
-	'blasian': crawlBeatstars('169836', 'musician'),
-	'nybangers': crawlAirbit('https://airbit.com/widgets/html5?uid=25700&config=201391')
+	'blackrose': () => crawlBeatstars('174557', 'musician'),
+	'blasian': () => crawlBeatstars('169836', 'musician'),
+	'nybangers': () => crawlAirbit('https://airbit.com/widgets/html5?uid=25700&config=201391')
 		.then(results =>
 			results.map(beat => {
 				beat.name = titleCase(beat.name);
@@ -48,10 +50,18 @@ const beatFetches = {
 		)
 };
 
+if (argv.hasOwnProperty('only')) {
+	const producers = argv.only.split(',').map(str => str.trim().toLowerCase());
+	for (const producer in beatFetches) {
+		if (!beatFetches.hasOwnProperty(producer)) continue;
+		if (!producers.includes(producer)) delete beatFetches[producer];
+	}
+}
+
 const fetchMap = [];
 for (const entry of Object.entries(beatFetches)) {
 	fetchMap.push(
-		entry[1]
+		entry[1]()
 			.then(item => {
 				fs.writeFile(`dumps/${entry[0]}.json`, JSON.stringify(item, null, '\t'));
 			})
